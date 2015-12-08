485492b59e1baf6470574733c10af733241c86b0
KimDarren <korean.darren@gmail.com>
2015-12-08 09:35:12 +0900

    change the contentoffset from CGPoint(0,0) to .zero

--- Carte/CarteViewController.swift
+++ Carte/CarteViewController.swift
@@ -154,7 +154,7 @@ public class CarteDetailViewController: UIViewController {
     public override func viewDidLoad() {
         self.view.backgroundColor = UIColor.whiteColor()
         self.textView.frame = self.view.bounds
-        self.textView.contentOffset = CGPoint(x: 0, y: 0)
+        self.textView.contentOffset = .zero
         self.view.addSubview(self.textView)
     }
 
