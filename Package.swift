c9da314e8968b17d5c7e1db14b8b273d26a667f9
Suyeol Jeon <devxoul@gmail.com>
2019-03-29 18:53:32 +0900

    Update Package.swift to support Swift 5

--- Package.swift
+++ Package.swift
@@ -1,14 +1,18 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
   name: "Carte",
+  platforms: [
+    .iOS(.v8),
+  ],
   products: [
     .library(name: "Carte", targets: ["Carte"]),
   ],
   targets: [
     .target(name: "Carte"),
     .testTarget(name: "CarteTests", dependencies: ["Carte"]),
-  ]
+  ],
+  swiftLanguageVersions: [.v4_2, .v5]
 )
